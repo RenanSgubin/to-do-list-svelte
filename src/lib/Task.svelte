73477a87f878446e2b85215a3f209dd855e9de83
= <renan08042004@gmail.com>
2022-06-13 19:32:27 -0300

    -Adicionado os arquivos ao projeto

---
+++ src/lib/Task.svelte
@@ -0,0 +1,47 @@
+
+<script lang="ts">
+    export let content: string;
+    export let completed: boolean;
+</script>
+
+<section class="task-content">
+    <span class="black-left-border"></span>
+    <input type="checkbox" bind:checked="{completed}" class="task-checkbox w-5 h-5"/>
+    <input type="text" bind:value="{content}" 
+    disabled={completed}
+    class="disabled:opacity-50 disabled:line-through"/> 
+</section>
+
+<style>
+
+.task-content {
+    position: relative;
+    width: 100%;
+    height: 6vh;
+    background-color: rgb(255, 255, 255);
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    margin-top: 1%;
+    border-radius: 3px;
+}
+
+.black-left-border {
+    position: absolute;
+    left: 0%;
+    height: 100%;
+    width: 1.5%;
+    background-color: rgb(40, 40, 40);
+    border-top-left-radius: 3px;
+    border-bottom-left-radius: 3px;
+}
+
+.task-checkbox {
+    margin-right: 7px;
+    margin-left: calc(1.5% + 20px);
+    border-radius: 10px;
+    border: 0;
+    outline-style: none;
+}
+
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,85 @@
+
+<script lang="ts">
+    import { text } from "svelte/internal";
+    import Task from "../lib/Task.svelte";
+
+    interface iTask {
+        text: string;
+        completed: boolean;
+    }
+
+    let tasks: iTask[] = [
+        {
+            text: "Minha tarefa",
+            completed: true
+        },
+        {
+            text: "Minha tarefa",
+            completed: false
+        }
+    ];
+
+    function addTask() {
+        tasks = [...tasks, {
+            text: "Minha task criada por função",
+            completed: false
+        }];
+    }
+
+</script>
+
+<section class="app-container">
+    <div class="app-content">
+        {#each tasks as task}
+            <Task bind:content={task.text} bind:completed={task.completed}/>
+        {/each}
+        <button class="add-task" on:click="{addTask}">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <span>Add Task</span>
+        </button>
+    </div>
+</section>
+
+
+<style>
+    .app-container {
+        width: 100%;
+        height: 100vh;
+        background-color: rgb(147, 195, 237);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .app-content {
+        width: 450px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .add-task {
+        width: 100%;
+        height: 7vh;
+        background-color: rgb(124, 185, 238);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border: 2px black dashed;
+        margin-top: 2%;
+        border-radius: 5px;
+        color: rgb(232, 232, 232);
+    }
+
+    .add-task:hover {
+        color: rgb(246, 246, 246);
+    }
+
+    .add-task span {
+        margin-left: 7px;
+    }
+</style>
